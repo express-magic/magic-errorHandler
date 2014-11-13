4ffec5b1918240cb109f6b983ce6eb6b792ad18c
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-13 17:50:26 +0100

    added 404 handler, added logging

--- errorHandler.js
+++ errorHandler.js
@@ -6,10 +6,16 @@ var express = require('express')
   , log     = require('magic-log')
 ;
 
+handler.use(function (req, res, next) {
+  log.warn('404 called');
+  res.send('404 error');
+}
+
 handler.use(function(err, req, res, next) {
   if ( err.status === 404 ) {
     return res.send('404 error');
   }
+  log.warn('500 called');
   res.send('error');
 });
 
