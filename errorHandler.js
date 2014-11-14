ab588d027c2c8c08e8f05588c3dd56cf0011cf71
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-14 04:23:12 +0100

    first part of rebuild

--- errorHandler.js
+++ errorHandler.js
@@ -1,22 +1,21 @@
 'use strict';
-
 var express = require('express')
   , path    = require('path')
-  , handler = express()
+  , handler = express.Router()
   , log     = require('magic-log')
 ;
-
 handler.use(function (req, res, next) {
-  log.warn('404 called');
-  res.send('404 error');
-}
+  var err = { status: 404 };
+  log.error('404 called');
+  next(err, req, res, next);
+});
 
 handler.use(function(err, req, res, next) {
   if ( err.status === 404 ) {
+    log.error('404 called');
     return res.send('404 error');
   }
-  log.warn('500 called');
-  res.send('error');
+  log.error('500 called, err:', err);
+  res.send('500 server error');
 });
-
 module.exports = handler;
