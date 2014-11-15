2d16748b7f7175f6fbf18e397f27dcb73e13d6d6
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-15 03:49:18 +0100

    added redirect if set in host

--- errorHandler.js
+++ errorHandler.js
@@ -8,12 +8,17 @@ var express = require('express')
 
 handler.use(function (req, res, next) {
   var app  = req.app
-    , r404 = app.get('404page') || '404'
+    , p404 = app.get('404page') || '404'
+    , r404 = app.get('404redirect') || false
   ;
-  log.warn('404 error page called, page was:', req.params.page);
-  req.params.page = r404;
-  res.status(404);
+  if ( r404 ) { 
+    log('magic-errorHandler', 'r404 was set in host, redirect:', r404);
+    return res.redirect(r404); 
+  }
 
+  log.warn('404 error page called, page was:', req.params.page);
+  req.params.page = p404;
+  res.status(404);
   view.render.page(req, res, function(err) {
     log.error('404 page template for host not found');
     if ( err ) { next(err); }
