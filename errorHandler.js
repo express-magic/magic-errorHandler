bdeee3ca17c0445bef710adc954aac76dc79cb99
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-15 04:39:26 +0100

    moved error handling into two separate functions

--- errorHandler.js
+++ errorHandler.js
@@ -1,17 +1,15 @@
 'use strict';
 var express = require('express')
-  , path    = require('path')
-  , handler = express()
+  , handler = express.Router()
   , log     = require('magic-log')
   , view   = require('magic-view')
 ;
-
-handler.use(function (req, res, next) {
+exports.handle404 = function (req, res, next) {
   var app  = req.app
     , p404 = app.get('404page') || '404'
     , r404 = app.get('404redirect') || false
   ;
-  if ( r404 ) { 
+  if ( r404 ) {
     log('magic-errorHandler', 'r404 was set in host, redirect:', r404);
     return res.redirect(r404); 
   }
@@ -23,11 +21,8 @@ handler.use(function (req, res, next) {
     log.error('404 page template for host not found');
     if ( err ) { next(err); }
   });
-});
-
-handler.use(function(err, req, res, next) {
+}
+exports.handle500 =  function(err, req, res, next) {
   log.error('500 called, err:', err);
   res.send('500 server error.');
-});
-
-module.exports = handler;
+}
